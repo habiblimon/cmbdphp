4400f3e538fdb2d190189386423ee754ef645c0c
habiblimon <md.habiblimon94@gmail.com>
2023-12-11 00:03:01 +0600

    22.18 Array (index, associative

---
+++ array.php
@@ -0,0 +1,19 @@
+<?php
+//index array
+
+$studentNames = ["Rahim", "Karim", "Rafiq", "jabbar", "kamal"];
+
+
+echo "<pre>";
+var_dump($studentNames);
+
+print_r($studentNames);
+
+echo gettype($studentNames);
+echo "</pre>";
+
+
+
+
+
+?>
