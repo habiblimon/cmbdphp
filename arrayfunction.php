aad0261b98d7d0c21a221a032aee3c7274374d53
habiblimon <md.habiblimon94@gmail.com>
2023-12-14 00:41:08 +0600

    57.mnt array password

---
+++ arrayfunction.php
@@ -0,0 +1,83 @@
+<?php
+# array function 
+
+
+$names  = array ("kamal", "jamal", "Tomal");
+
+print_r($names);
+
+echo "<br>";
+#is_array
+
+$cities = ["Dhaka"];
+
+is_array($cities);
+
+var_dump(is_array($cities));
+
+echo "<br>";
+
+#in_array
+in_array("Kamal", $names);
+
+var_dump(in_array("kamal", $names));
+
+echo "<br>";
+
+#array_marge 
+
+$NameAndCities = array_merge($names, $cities);
+
+print_r($NameAndCities);
+echo "<br>";
+
+#array_keys  - Assosiative array
+
+$myInfon = [
+    "Name  " => "Habib",
+    "Gender" => "Male",
+    "City  " => "Chittagong"
+];
+
+$allKyes = array_keys($myInfon);
+
+print_r($allKyes);
+echo "<br>";
+
+#array_key_exists
+
+array_key_exists("City", $myInfon);
+
+var_dump(array_key_exists("city", $myInfon));
+
+#array_shift    => Remove 1st array value .
+#array_unshift  => Replace 1st array value + adding more value next step by step.
+#array_push     => add new value in array items at last.
+#array_pop      => last value remove from array.
+#array_value    => 
+#array_map      
+
+array_map(function($n){
+echo $n."<br>";
+}, $names);
+
+
+#array_uniq
+
+$persons = ["habib", "limon", "Humaira", "Abiba"];
+$unique_person = array_unique($persons);
+echo "<br>";
+
+print_r($unique_person);
+
+#array_slice()
+#array_diff() 
+#array_search()
+#array_reserse()
+
+
+
+
+
+
+?>
--- arrayproblems.php
+++ arrayproblems.php
@@ -11,7 +11,7 @@ echo "Maximum number :". $max."<br>";
 rsort($numbers);
 
 $uniqueNumber = array_unique($numbers);
-$max2 = array_slice($uniqueNumber, 1, 1);
+$max2 = array_slice($uniqueNumber, 1, 3);
 echo "2nd Max Number : ".$max2[0]."<br>";
 
 #sort array from min to max
@@ -19,12 +19,30 @@ echo "2nd Max Number : ".$max2[0]."<br>";
 $smallnum = [2,3,4,1];
 
 sort($smallnum);
+print_r($smallnum)."<br>";
 
-echo sort($smallnum)."<br>";
-
+echo "<br>";
 
 #calculate average number of an array.
-$avg = 
+
+$avg = array_sum($smallnum)/count($smallnum);
+echo $avg."<br>";
+
+#search data from array
+#lowercase  >> strtolower();
+#upercase  >> strtoupper();
+#remove duplicate 
+#array_slice()  
+
+$userName = 'Habib';
+$exisitingUser = ["limon Habib", "Habib Limon"];
+$isUnique = !in_array($userName, $exisitingUser);
+echo $isUnique ?'UserName is Unique' : 'UserName is exists';
+
+
+
+
+
 
 
 ?>
